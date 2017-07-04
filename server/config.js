9678a6854e72468382d9d3a37d302c7ab6a0fc64
Reuven Chacha <reuvenchacha@gmail.com>
2017-07-04 18:39:30 +0300

    Minor change

--- server/config.js
+++ server/config.js
@@ -4,10 +4,6 @@
 const path = require('path');
 const twitterCredentials = require('../twitter-credentials.json');
 
-if (!twitterCredentials.consumerKey || !twitterCredentials.consumerSecret) {
-	throw new Error('Please provide valid twitter credentials in twitter-credentials.json: consumerKey, consumerSecret');
-}
-
 const staticContentLocation = path.join(__dirname, '../build');
 
 /**
@@ -22,10 +18,14 @@ const staticContentLocation = path.join(__dirname, '../build');
  * 	}} AppConfig
  */
 module.exports = {
-	staticContentLocation,
-	callbackPath: 'auth/callback',
-	port: process.env.PORT || '3002',
-	templatePath: path.join(staticContentLocation, 'index.html'),
-	twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY || twitterCredentials.consumerKey,
-	twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET || twitterCredentials.consumerSecret,
+    staticContentLocation,
+    callbackPath: 'auth/callback',
+    port: process.env.PORT || '3002',
+    templatePath: path.join(staticContentLocation, 'index.html'),
+    twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY || twitterCredentials.consumerKey,
+    twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET || twitterCredentials.consumerSecret,
 };
+
+if (!module.exports.twitterConsumerKey || !module.exports.twitterConsumerSecret) {
+    throw new Error('Please provide valid twitter credentials in twitter-credentials.json: consumerKey, consumerSecret');
+}
